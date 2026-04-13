a83e7028cbddb44bb24e73b7ed41803f9ef27b50
twickers999 <vlad.inbox1999@gmail.com>
2026-04-13 11:15:56 +0100

    Add Instruction and CompassDirection enums

---
+++ MarsRover.TerminalApp/InputEnums.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarsRover.TerminalApp
+{
+    public  class InputEnums
+    {
+        public enum Instruction
+        {
+            L,
+            R,
+            M
+        }
+
+        public enum CompassDirection
+        {
+            N,
+            E,
+            S,
+            W
+        }
+    }
+}
